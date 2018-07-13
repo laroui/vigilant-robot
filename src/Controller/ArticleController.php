bd250a1a1aa94bc4816f47f17a1537f244649b8b
laroui <l.nassimo@gmail.com>
2018-07-13 21:38:32 +0200

    First commit

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,7 +9,28 @@
 namespace App\Controller;
 
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+
 class ArticleController
 {
+    /**
+     * @Route("/")
+     */
+    public function homepage(){
+
+        return new Response('OmG my first page already');
+    }
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug){
+
+        return new Response(sprintf('future page to display article :%s',$slug));
+
+
+    }
 
 }
+
